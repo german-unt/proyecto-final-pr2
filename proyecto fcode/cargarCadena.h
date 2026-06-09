97eaa19b9aa20ad0628c2e2c6f24c25401b7785c
nachogonzalez912 <ignafagon@gmail.com>
2026-06-08 22:37:58 -0300

    listados y informes
    ...

--- proyecto fcode/cargarCadena.h
+++
@@ -1,6 +0,0 @@
-#ifndef CARGARCADENA_H_INCLUDED
-#define CARGARCADENA_H_INCLUDED
-
-void cargarCadena(char *, int);
-
-#endif // CARGARCADENA_H_INCLUDED
--- proyecto fcode/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "funciones.h"
-using namespace std;
-
-int main()
-{
-menuPrincipal();
-    return 0;
-}
